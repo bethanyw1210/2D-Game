b5463b584e539af385d125aea1192588fd949d74
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-27 23:20:47 -0600

    added animation to shark and started working on coding

--- 2D-Game-Bethany-Woodruff/Assets/Scripts/Enemy Script/EnemyMovement.cs
+++ 2D-Game-Bethany-Woodruff/Assets/Scripts/Enemy Script/EnemyMovement.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
+    [SerializeField] float walkSpeed = 8f;
+    public bool moveRight;
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+    private bool notAtEdge;
+    public Transform edgeCheck;
 
-	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position,wallCheckRadius,whatIsWall);
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position,wallCheckRadius,whatIsWall);
+
+        if(hittingWall || !notAtEdge)
+        {
+            moveRight = !moveRight;
+        }
+        if(moveRight)
+        {
+        transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
+        }
+        else
+        {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        }
+
+    }
 }
