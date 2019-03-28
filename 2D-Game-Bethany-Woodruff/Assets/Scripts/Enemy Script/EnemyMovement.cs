99a872f717c57e6a274196ae7b87f1fe42b4a614
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-27 23:44:42 -0600

    finished coding for shark and swordfish going across screen

--- 2D-Game-Bethany-Woodruff/Assets/Scripts/Enemy Script/EnemyMovement.cs
+++ 2D-Game-Bethany-Woodruff/Assets/Scripts/Enemy Script/EnemyMovement.cs
@@ -2,36 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement:MonoBehaviour
+{
 
-    [SerializeField] float walkSpeed = 8f;
-    public bool moveRight;
-    public Transform wallCheck;
-    public float wallCheckRadius;
-    public LayerMask whatIsWall;
-    private bool hittingWall;
-    private bool notAtEdge;
-    public Transform edgeCheck;
+    private bool dirRight = true;
+    public float speed = 5f;
+    private bool flipX = false;
 
-	// Update is called once per frame
-	void Update () {
-
-        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position,wallCheckRadius,whatIsWall);
-
-        hittingWall = Physics2D.OverlapCircle(wallCheck.position,wallCheckRadius,whatIsWall);
-
-        if(hittingWall || !notAtEdge)
-        {
-            moveRight = !moveRight;
-        }
-        if(moveRight)
-        {
-        transform.Translate(Vector2.right * walkSpeed * Time.deltaTime);
-        }
+    void Update()
+    {
+        if(dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
         else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+
+        if(transform.position.x > 14.5f)
         {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+            dirRight = false;
+            GetComponent<SpriteRenderer>().flipX = true;
         }
 
+        if(transform.position.x <= -12)
+        {
+            dirRight = true;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        /*[SerializeField] float swimSpeed = 8f;
+        public bool moveRight;
+        public Transform wallCheck;
+        public float wallCheckRadius;
+        public LayerMask whatIsWall;
+        private bool hittingWall;
+        private bool notAtEdge;
+        public Transform edgeCheck;
+
+        // Update is called once per frame
+        void Update () {
+
+            notAtEdge = Physics2D.OverlapCircle(edgeCheck.position,wallCheckRadius,whatIsWall);
+
+            hittingWall = Physics2D.OverlapCircle(wallCheck.position,wallCheckRadius,whatIsWall);
+
+            if(hittingWall || !notAtEdge)
+            {
+                moveRight = !moveRight;
+            }
+            if(moveRight)
+            {
+            transform.Translate(Vector2.right * swimSpeed * Time.deltaTime);
+            }
+            else
+            {
+            transform.Translate(Vector2.left * swimSpeed * Time.deltaTime);
+            }
+
+        }*/
     }
 }
