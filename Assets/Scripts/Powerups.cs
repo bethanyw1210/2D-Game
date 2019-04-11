5d852c45583b687041fcbaf73fd1e02fc65fa7e1
Bethany Woodruff <bethany.w1210@gmail.com>
2019-04-10 22:02:21 -0600

    Working on powerups script

--- Assets/Scripts/Powerups.cs
+++ Assets/Scripts/Powerups.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Powerups : MonoBehaviour {
 
-    public int coinValue;
+    public GameObject turtlePowerup;
+    private float lastXPosition, lastYPosition, edgeDistance = 10f;
+
 
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(gameObject.tag == "Fish")
-            print("You've collected a coin!");
+        if(gameObject.tag == "Turtle")
+            Destroy(gameObject);
+        GetComponent<SpriteRenderer>().sprite = ("TurtlePowerup");
 
-        Destroy(gameObject);
+        if(!(edgeDistance < lastXPosition) && !(-edgeDistance > lastXPosition))
+        {
+            GameObject turtlePowerup = Instantiate(turtlePowerup,new Vector2(lastXPosition + lastYPosition,gameObject.transform.rotation));
+            lastYPosition = GameObject.Find("Fish Character").transform.position.x;
+            lastYPosition = GameObject.Find("Fish Character").transform.position.y;
+        }
+
+
     }
 }
