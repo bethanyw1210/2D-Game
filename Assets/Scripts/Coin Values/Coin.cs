c529284f89c6d8cb122d56118de66a71071a5827
Bethany Woodruff <bethany.w1210@gmail.com>
2019-04-26 19:05:17 -0600

    fixed coin script

--- Assets/Scripts/Coin Values/Coin.cs
+++ Assets/Scripts/Coin Values/Coin.cs
@@ -6,36 +6,11 @@ using UnityEngine.UI;
 public class Coin : MonoBehaviour {
 
     public int coinValue;
-    public int coinAmount;
-    public int isLifeBought;
-    public Button LifeButton;
-
-    public void Start()
-    {
-        coinAmount = PlayerPrefs.GetInt("CoinAmount");
-    }
-    
-    void Update()
-    {
-        isLifeBought = PlayerPrefs.GetInt("IsLifeBought");
-
-        if(coinAmount >= 10 && isLifeBought == 0)
-            LifeButton.interactable = true;
-        else
-            LifeButton.interactable == false;
-    }
-
-    public void buyLife()
-    {
-        coinAmount -= 10;
-        PlayerPrefs.SetInt("IsLifeBought",1);
-        Debug.Log("Life Bought!");
-    }
 
     //Coin value and destroy coin 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(GameObject.FindGameObjectWithTag("Player"))
+        if(GameObject.FindGameObjectWithTag("Fish"))
             print("You've collected a coin!");
 
         ScoreManager.AddPoints(coinValue);
