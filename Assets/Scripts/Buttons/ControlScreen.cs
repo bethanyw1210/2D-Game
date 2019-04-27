2cc4dab7bce7f5e667c619527e4be12b6886beb1
Bethany Woodruff <bethany.w1210@gmail.com>
2019-04-26 20:07:33 -0600

    Added controls screen

---
+++ Assets/Scripts/Buttons/ControlScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlScreen : MonoBehaviour {
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("ControlScreen");
+    }
+}
